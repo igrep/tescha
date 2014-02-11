4b53d298ed0a56ef624f1733e30eb69aba0e7a22
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-02-11 21:41:34 +0900

    add a test case of empty result set

--- lib/tescha/result_set.rb
+++ lib/tescha/result_set.rb
@@ -1,4 +1,27 @@
 module Tescha
   class ResultSet
+    def initialize results
+      @failures = []
+      @results = []
+    end
+    def to_s
+      "#{@results.length} examples, #{@failures.length} failures."
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  this_file = File.readlines( __FILE__ )
+
+  puts 'An empty result set'
+  instance_in_test = Tescha::ResultSet.new( [] )
+
+  assertion_line = this_file[ __LINE__ ].sub( /if/, ''.freeze ).strip
+  if ( actual = instance_in_test.to_s ) == ( expected = "0 examples, 0 failures." )
+    puts 'OK'
+  else
+    fail "Assertion failed in '#{assertion_line}'\n" \
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
   end
 end
