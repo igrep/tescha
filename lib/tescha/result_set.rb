70f66cfc2aaa4bb6f8225091566506b2fa0b3940
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-02-28 19:59:23 +0900

    add use case of a result set with some tests
    I have no idea of the result so far.

--- lib/tescha/result_set.rb
+++ lib/tescha/result_set.rb
@@ -13,14 +13,31 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   require 'tescha/meta_test'
+  include Tescha
 
   puts 'An empty result set'
-  instance_in_test = Tescha::ResultSet.new( [] )
+  instance_in_test = ResultSet.new
 
-  Tescha::MetaTest.test(
+  MetaTest.test(
     "prints only the count of tests, assertions, and failed assertions.",
     ( actual = instance_in_test.to_s ) == ( expected = "0 tests, 0 assertions, 0 failed assertions." ),
       "The expected value: #{expected.inspect}\n" \
       "The actual value:   #{actual.inspect}"
+  )
+
+  puts 'A result set with some results'
+  instance_in_test = ResultSet.new
+  instance_in_test.add(
+    Test.new( 'test1', [
+      Assertions.new( 1, [ :==, 1 ] ),
+      Assertions.new( 1, [ :==, 0 ] ),
+      Assertions.new( '', [ :empty? ] ),
+    ] )
+  )
+  instance_in_test.add(
+    Test.new( 'test2', [
+      Assertions.new( 'foo', [ :==, 'bar' ] ),
+      Assertions.new( nil, [ :nil? ] ),
+    ] )
   )
 end
