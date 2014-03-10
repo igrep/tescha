fdd89cd3021308868868a1985e9a0bc790668b65
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-03-10 20:43:19 +0900

    add Assertion's spec and rough implementation

--- lib/tescha/assertion.rb
+++ lib/tescha/assertion.rb
@@ -6,13 +6,10 @@ module Tescha
       @method_name = method_name
       @args = args
 
-      @result = evaluate( object.__send__ method_name, *args )
+      @result = object.__send__ method_name, *args
 
-      if @result == OK
+      if @result
       end
-    end
-
-    def evaluate expression
     end
 
     def to_s
@@ -20,4 +17,41 @@ module Tescha
     end
 
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  require 'tescha/meta_test'
+  include Tescha
+
+  puts "given a expression returning true"
+  instance_in_test = Assertion.new 1, :==, 1
+  MetaTest.test( 'its result is SUCCESSFUL',
+    ( actual = instance_in_test.result ) == ( expected = Assertion::SUCCESSFUL ),
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
+  )
+
+  puts "given a expression returning a truthy value"
+  instance_in_test = Assertion.new ['a'], :first
+  MetaTest.test( 'its result is SUCCESSFUL',
+    ( actual = instance_in_test.result ) == ( expected = Assertion::SUCCESSFUL ),
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
+  )
+
+  puts "given a expression returning false"
+  instance_in_test = Assertion.new :a, :==, :b
+  MetaTest.test( 'its result is failed',
+    ( actual = instance_in_test.result ) == ( expected = Test::FAILED ),
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
+  )
+
+  puts "given a expression returning nil"
+  instance_in_test = Assertion.new {}, :[], :non_exisitng_key
+  MetaTest.test( 'its result is failed',
+    ( actual = instance_in_test.result ) == ( expected = Test::FAILED ),
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
+  )
 end
