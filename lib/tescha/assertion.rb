50d989b372cf73a9f07f0bcc34b8403a84af5395
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-03-27 08:26:42 +0800

    make Assertion return the shared constants as its result

--- lib/tescha/assertion.rb
+++ lib/tescha/assertion.rb
@@ -1,20 +1,18 @@
+require 'tescha/test/constants'
+
 module Tescha
   class Assertion
 
-    attr_reader :result_message
+    attr_reader :result_message, :as_result
 
     def initialize object, method_name, args = []
-      @result = object.__send__ method_name, *args
-      @successful = !!@result
+      result = object.__send__ method_name, *args
+      @as_result = result ? Test::SUCCESSFUL : Test::FAILED
       @result_message =
-        unless @successful
+        unless result
           "Assertion failed.\n" \
-            "The expression #{object.inspect}.#{method_name}(#{args.map( &:inspect ).join( ', ' )}) returned #{@result.inspect}!\n"
+            "The expression #{object.inspect}.#{method_name}(#{args.map( &:inspect ).join( ', ' )}) returned #{result.inspect}!\n"
         end
-    end
-
-    def successful?
-      @successful
     end
 
   end
@@ -27,7 +25,7 @@ if __FILE__ == $PROGRAM_NAME
   puts "given a expression returning true"
   instance_in_test = Assertion.new 1, :==, [1]
   MetaTest.test( 'its result is successful',
-    ( actual = instance_in_test.successful? ) == ( expected = true ),
+    ( actual = instance_in_test.as_result ) == ( expected = Test::SUCCESSFUL ),
       "The expected value: #{expected.inspect}\n" \
       "The actual value:   #{actual.inspect}"
   )
@@ -39,7 +37,7 @@ if __FILE__ == $PROGRAM_NAME
   puts "given a expression returning a truthy value"
   instance_in_test = Assertion.new ['a'], :first
   MetaTest.test( 'its result is successful',
-    ( actual = instance_in_test.successful? ) == ( expected = true ),
+    ( actual = instance_in_test.as_result ) == ( expected = Test::SUCCESSFUL ),
       "The expected value: #{expected.inspect}\n" \
       "The actual value:   #{actual.inspect}"
   )
@@ -51,7 +49,7 @@ if __FILE__ == $PROGRAM_NAME
   puts "given a expression returning false"
   instance_in_test = Assertion.new :a, :==, [:b]
   MetaTest.test( 'its result is failed',
-    ( actual = instance_in_test.successful? ) == ( expected = false ),
+    ( actual = instance_in_test.as_result ) == ( expected = Test::FAILED ),
       "The expected value: #{expected.inspect}\n" \
       "The actual value:   #{actual.inspect}"
   )
@@ -66,7 +64,7 @@ if __FILE__ == $PROGRAM_NAME
   puts "given a expression returning nil"
   instance_in_test = Assertion.new( {}, :[], [:non_exisitng_key] )
   MetaTest.test( 'its result is failed',
-    ( actual = instance_in_test.successful? ) == ( expected = false ),
+    ( actual = instance_in_test.as_result ) == ( expected = Test::FAILED ),
       "The expected value: #{expected.inspect}\n" \
       "The actual value:   #{actual.inspect}"
   )
