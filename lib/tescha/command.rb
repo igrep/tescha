aca80434b43db0411a2afd23f66f52597ee4d630
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-06-20 08:13:01 +0900

    add test of Tescha::Command for simple cases

--- lib/tescha/command.rb
+++ lib/tescha/command.rb
@@ -37,11 +37,29 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   require 'tescha/meta_test'
+  include Tescha
 
   project_root = File.join File.dirname(__FILE__), '../..'
-  tescha_command = File.expand_path File.join(project_root, 'bin/tescha')
+  tescha_command = "ruby #{File.expand_path File.join(project_root, 'bin/tescha')}"
   fixtures_directory = File.join(project_root, 'test/tescha/command/fixtures')
   Dir.chdir fixtures_directory do
-    p system('ruby', tescha_command) # check if executable
+    puts "Given no argument"
+    MetaTest.test('it should be executable and print out nothing.',
+      system(tescha_command),
+      'Actually something happened. see the error message'
+    )
+
+    puts "Given file names"
+    expected = [
+      "Test Ruby file 1: true",
+      "Test Ruby file 2: true",
+      "Test Ruby file 3: true",
+    ]
+    MetaTest.test( 'it executes the given files making Tescha.ready? return true.',
+      ( (actual = `#{tescha_command} test?.rb`.lines.map(&:chomp).sort!) == expected.sort! ),
+      "The expected value: #{expected.inspect}\n" \
+      "The actual value:   #{actual.inspect}"
+    )
+
   end
 end
