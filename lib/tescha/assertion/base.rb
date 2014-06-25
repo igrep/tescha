610cb1672a97883d82edd4e3686e971a2064bf29
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-06-26 08:50:21 +0900

    copied Assertion to Assertion::Base

---
+++ lib/tescha/assertion/base.rb
@@ -0,0 +1,24 @@
+require 'tescha/test/constants'
+
+module Tescha
+  module Assertion
+    class Base
+      attr_reader :result_message, :as_result
+
+      def initialize object, method_name, args = []
+        result = object.__send__ method_name, *args
+        @as_result = successful?(result) ? Test::SUCCESSFUL : Test::FAILED
+        @result_message =
+          unless result
+            "Assertion failed.\n" \
+              "The expression #{object.inspect}.#{method_name}(#{args.map( &:inspect ).join( ', ' )}) returned #{result.inspect}!\n"
+          end
+      end
+
+      def successful?
+        raise NotImplementedError
+      end
+
+    end
+  end
+end
